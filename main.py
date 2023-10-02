e6832544f06535af540e787490f63140abcae039
Muhammad Iqbal <miqbal020@gmail.com>
2023-10-03 03:29:08 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def is_palindrome(s):
+    s = s.lower()
+    s = ''.join(filter(str.isalnum, s))
+    return s == s[::-1]
+
+
+def main():
+    try:
+        number = input('Masukan Nilai: ')
+        if is_palindrome(number):
+            print(f'{number} is palindrome')
+        else:
+            print(f'{number} is not palindrome')
+    except:
+        print('Input Invalid')
+
+
+if __name__ == '__main__':
+    print(121 % 10)
+    main()
